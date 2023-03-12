22c017f267c075c8cde5636d6df20130c87d5228
Mauricio1408 <126628017+Mauricio1408@users.noreply.github.com>
2023-03-12 09:06:18 +0800

    Update Activity 1 - Bresenham line.py

--- pages/Activity 1 - Bresenham line.py
+++ pages/Activity 1 - Bresenham line.py
@@ -3,14 +3,14 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-st.title("BRESENHAM LINE")
-st.ylabel("Y-Axis")
-st.xlabel("X-Axis")
+plt.title("BRESENHAM LINE")
+plt.ylabel("Y-Axis")
+plt.xlabel("X-Axis")
 
 def Midpoint(x, y, x2, y2):
     xMid = (x + x2)/2
     yMid = (y + y2)/2
-    st.plot(int(xMid), int(yMid), 'b.')
+    plt.plot(int(xMid), int(yMid), 'b.')
     st.write('The midpoint is: ', xMid, yMid)
 
 def BRESENHAMS_LINE(x1, y1, x2, y2, color):
@@ -40,8 +40,8 @@ def BRESENHAMS_LINE(x1, y1, x2, y2, color):
         X = (X_coordinates[0], X_coordinates[-1])
         Y = (Y_coordinates[0], Y_coordinates[-1])
 
-    st.plot(X, Y, 'lightblue', linewidth="1")
-    st.scatter(X_coordinates, Y_coordinates, color='BLACK', s=25)
+    plt.plot(X, Y, 'lightblue', linewidth="1")
+    plt.scatter(X_coordinates, Y_coordinates, color='BLACK', s=25)
 
 def main():
     st.write("Coordinates of First Point")
@@ -55,8 +55,8 @@ def main():
     color = 'b'
     BRESENHAMS_LINE(x, y, xEnd, yEnd, color)
     Midpoint(x, y, xEnd, yEnd)
-    st.grid()
-    st.show()
+    plt.grid()
+    plt.show()
 
 if __name__ == '__main__':
   main()
