f30407034abbd46491a1724a8936ed0338dcd079
Mauricio1408 <126628017+Mauricio1408@users.noreply.github.com>
2023-03-12 18:04:05 +0800

    Update streamlit_app.py

--- pages/streamlit_app.py
+++ pages/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import sys
-import tasks.py as task_one
+import pages.tasks as task_one
 
 st.title('Midterms in CC201')
 
@@ -10,6 +10,9 @@ y1 = st.sidebar.slider('y1')
 x2 = st.sidebar.slider('x2')
 y2 = st.sidebar.slider('y2')
 
+st.header("Task 1")
+st.subheader("Midpoint line")
+st.pyplot(task_one.MIDPOINT(x1,y1,x2,y2)
 
 
 
