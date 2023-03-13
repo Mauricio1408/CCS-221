239da4a1766dc384e316af0f534fa349cf9275cb
Mauricio1408 <126628017+Mauricio1408@users.noreply.github.com>
2023-03-13 14:47:43 +0800

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -6,14 +6,20 @@ import pages.tasks as task_one
 def main():
   st.title('Midterms in CC201')
 
-  x1 = st.sidebar.slider('x1', 1, 500), \
-  y1 = st.sidebar.slider('y1', 1, 500), \
-  x2 = st.sidebar.slider('x2', 1, 500), \
-  y2 = st.sidebar.slider('y2', 1, 500)
+  x1, y1, x2, y2 = st.sidebar.slider('x1', 1, 500), \
+  st.sidebar.slider('y1', 1, 500), \
+  st.sidebar.slider('x2', 1, 500), \
+  st.sidebar.slider('y2', 1, 500)
 
   st.header("Task 1")
   st.subheader("Midpoint line")
   st.pyplot(task_one.MIDPOINT(x1,y1,x2,y2))
+  
+  st.subheader("Bresenham line")
+  st.pyplot(task_one.BRESENHAMS_LINE(x1,y1,x2,y2))
+  
+#   st.subheader("DDA line")
+#   st.pyplot(task_one.MIDPOINT(x1,y1,x2,y2))
             
   if st.button("Exit"):
             st.stop()
