0432a6a175b4a7e1d13d827bbebe75f83194ebfe
Mauricio1408 <126628017+Mauricio1408@users.noreply.github.com>
2023-03-13 16:13:02 +0800

    Update tasks.py

--- pages/tasks.py
+++ pages/tasks.py
@@ -96,9 +96,10 @@ def DDALine(x1, y1, x2, y2, color):
 
     for i in range(0, int(steps + 1)):
         #Draw pixels
+        plt.plot(int(x1), int(y1), color)
         x1 += Xinc
         y1 += Yinc
-        plt.plot(int(x1), int(y1), color)
+        
 
     Midpoint(x1, y1, x2, y2)    
     return fig
