57760e532448c5179a3797f69c1d83372e437301
Mauricio1408 <126628017+Mauricio1408@users.noreply.github.com>
2023-03-13 14:49:28 +0800

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -7,9 +7,9 @@ def main():
   st.title('Midterms in CC201')
 
   x1, y1, x2, y2 = st.sidebar.slider('x1', 1, 500), \
-  st.sidebar.slider('y1', 1, 500), \
-  st.sidebar.slider('x2', 1, 500), \
-  st.sidebar.slider('y2', 1, 500)
+                st.sidebar.slider('y1', 1, 500), \
+                st.sidebar.slider('x2', 1, 500), \
+                st.sidebar.slider('y2', 1, 500)
 
   st.header("Task 1")
   st.subheader("Midpoint line")
