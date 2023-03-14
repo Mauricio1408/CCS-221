13890250b15054cfbd4f8463c069a66229321fec
Mauricio1408 <126628017+Mauricio1408@users.noreply.github.com>
2023-03-14 15:31:03 +0800

    Update tasks2.py

--- pages/tasks2.py
+++ pages/tasks2.py
@@ -16,7 +16,7 @@ def change_boundary_fill(x_val, y_val, c, boarder):
     
     
 def boundary_fill(x_val, y_val, c, boarder):
-  fig - plt.figure()
+  fig = plt.figure()
   change_boundary_fill(x_val, y_val, c, boarder)
 
   img = plt.imshow(two_d_arr, interpolation = 'none', cmap = 'BrBG')
