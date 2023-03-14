d2176c94e7fd6a37fa78d9420cc1513d40518561
Mauricio1408 <126628017+Mauricio1408@users.noreply.github.com>
2023-03-14 15:11:58 +0800

    Update tasks2.py

--- pages/tasks2.py
+++ pages/tasks2.py
@@ -16,13 +16,13 @@ def change_boundary_fill(x_val, y_val, c, boarder):
     
     
 def boundary_fill(x_val, y_val, c, boarder):
-  
+  fig - plt.figure()
     change_boundary_fill(x_val, y_val, c, boarder)
 
     img = plt.imshow(two_d_arr, interpolation = 'none', cmap = 'BrBG')
     img.set_clim([0,50])    
     plt.colorbar()
-   return img
+  return fig
 
 
 #flood_fill_algorithm
